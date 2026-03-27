437e481330013ede8f2b5f2884859b0a9328c40f
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-03-27 13:37:23 +0000

    Feat(ui): Used Zudstand for managing local / client state

---
+++ src/store/useCartStore.tsx
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+
+export type Cart = {
+  id: number;
+  name: string;
+  quantity: number;
+  description: string;
+  price: number;
+  image: string;
+};
+
+export interface CartType {
+  item: Cart[];
+  addCart: (product: Cart) => void;
+  removeCart: (id: number) => void;
+  clearCart: () => void;
+  increaseQty: (id: number) => void;
+  decreaseQty: (id: number) => void;
+}
+
+export const useCartStore = create<CartType>()((set) => ({
+  item: [],
+
+  addCart: (product) =>
+    set((state) => {
+      const exist = state.item.find((i) => i.id === product.id);
+
+      if (exist) {
+        return {
+          item: state.item.map((i) =>
+            i.id === product.id ? { ...i, quantity: i.quantity + 1 } : i,
+          ),
+        };
+      }
+      return {
+        item: [...state.item, { ...product, quantity: 1 }],
+      };
+    }),
+
+  removeCart: (id) =>
+    set((state) => ({
+      item: state.item.filter((i) => i.id !== id),
+    })),
+
+  increaseQty: (id) =>
+    set((state) => {
+      const updatedItems = state.item.map((i) =>
+        i.id === id ? { ...i, quantity: i.quantity + 1 } : i,
+      );
+      return { item: updatedItems };
+    }),
+  decreaseQty: (id) =>
+    set((state) => {
+      const updateItems = state.item.map((i) =>
+        i.id === id
+          ? { ...i, quantity: i.quantity > 1 ? i.quantity - 1 : i.quantity }
+          : i,
+      );
+      return { item: updateItems };
+    }),
+
+  clearCart: () => set({ item: [] }),
+}));
