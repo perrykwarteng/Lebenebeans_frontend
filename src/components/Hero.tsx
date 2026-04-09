7e1272ebd27928d66c4ed32da04f9c55ed2e38a0
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 13:27:02 +0000

    Fix: Removed unwanted elements and heading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,42 @@
 import HeroImg from "../assets/images/hero.png";
+import { CgMenuBoxed } from "react-icons/cg";
 import { Button } from "./ui/Button";
 import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
-    <div className="h-full bg-white py-20">
-      <div className="px-8 md:px-16 md:pt-7 flex flex-col md:flex-row md:items-center">
+    <div className="min-h-screen bg-white py-20">
+      <div className="h-full px-8 md:px-16 md:pt-7 flex flex-col md:flex-row md:items-center">
         <motion.div
           className="flex-1"
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
         >
-          <div className="">
-            <p className="text-gray-600 text-[20px] italic mt-2.5">
-              Welcome to Lebenebeans
-            </p>
-            <h2 className="text-[45px] md:text-[55px] mt-4 font-semibold leading-12 md:leading-16">
-              Taste the Flavor of <br />
-              <span className="text-primary"> Home Fresh </span>& <br /> Hot
-              Meal
+          <div className="mt-14 md:12">
+            <h2 className="text-[39px] md:text-[55px]  font-semibold leading-12 md:leading-16">
+              Order your<span className="text-primary"> Favourite Foods </span>
+              at Lebene Beans
             </h2>
-            <p className="text-gray-500 mt-5">
-              Deliciously prepared meals made with love, rich spices, and the
-              finest ingredients bringing you the true taste of African cuisine
-              in every bite. Order your favourite foods from the comfort of your
-              homes with a very fast and quality delivery service
+            <p className="text-gray-500 text-[20px]">
+              from the comfort of your homes.
             </p>
 
             <div className=" flex flex-col gap-y-2 md:gap-x-2 mt-5 md:flex-row md:items-center ">
               <div
-                className="action"
+                className="flex items-center gap-x-2 bg-secondary text-white px-5 py-2 text-[19px] text-center rounded-xl w-45"
                 onClick={(e) => {
                   e.preventDefault();
                   const section = document.querySelector("#menu");
                   section?.scrollIntoView({ behavior: "smooth" });
                 }}
               >
-                <Button text="Check Menu" type="Submit" />
+                <CgMenuBoxed className="mt-1 md:mt-0" />
+                <Button
+                  text="Check Menu"
+                  type="Submit"
+                  Stlye="bg-transparent"
+                />
               </div>
             </div>
           </div>
@@ -49,7 +48,7 @@ export const Hero = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
         >
-          <img className="w-125 mt-4 md:mt-0" src={HeroImg} alt="Hero Image" />
+          <img className="w-125 mt-4 md:mt-6" src={HeroImg} alt="Hero Image" />
         </motion.div>
       </div>
     </div>
