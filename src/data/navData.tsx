5fcaa4d8bcfb001638378356230f10c42105ad92
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 13:29:34 +0000

    Fix: Reoved unused data

--- src/data/navData.tsx
+++ src/data/navData.tsx
@@ -1,21 +1,9 @@
-import { IoHome } from "react-icons/io5";
 import { CgMenuBoxed } from "react-icons/cg";
-// import { FiShoppingCart } from "react-icons/fi";
 
 export const navData = [
-  {
-    navLink: "/",
-    navText: "Home",
-    navIcon: <IoHome />,
-  },
   {
     navLink: "#menu",
     navText: "Menu",
     navIcon: <CgMenuBoxed />,
   },
-  // {
-  //   navLink: "/orders",
-  //   navText: "Orders",
-  //   navIcon: <FiShoppingCart />,
-  // },
 ];
