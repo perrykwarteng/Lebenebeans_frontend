38a4454abdcf520689284ea1fe554eb11e0c785e
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 13:39:32 +0000

    Feat: service file for auth endpoint calls

--- src/features/Auth/services.ts
+++ src/features/Auth/services.ts
@@ -1,8 +1,22 @@
-import axios from "axios";
+import type { loginType } from "./Login";
+import { api } from "../../lib/axios";
+
+export const login = async (data: loginType) => {
+  const res = await api.post(`/api/auth/login`, data);
+
+  if (res.data) {
+    const details = await api.get(`/api/auth/me`);
+
+    return details;
+  }
+};
+
+export const logout = async () => {
+  const res = await api.post(`/api/auth/logout`);
+  return res;
+};
 
 export const verify = async (ref: string) => {
-  const res = await axios.get(
-    `${import.meta.env.VITE_API_URL}/api/orders/verify/${ref}`,
-  );
+  const res = await api.get(`/api/orders/verify/${ref}`);
   return res.data.data || null;
 };
