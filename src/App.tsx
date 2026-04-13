198ab46290e57f512b31b547bae9cc07e87a324e
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-13 17:53:55 +0000

    Feat: Getting promotion code and type to get the promotion details and saving it in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
+import { useSearchParams } from "react-router-dom";
 import { Banner } from "./components/Banner";
 import { Hero } from "./components/Hero";
 import { MainLayout } from "./components/layouts/MainLayout";
 import { OpeningHours } from "./components/OpeningHours";
 import { Menu } from "./features/Menu/Menu";
+import { usePromoStore } from "./store/usePromoStore";
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getPromotion } from "./features/Cart/services";
+import { toast } from "sonner";
 
 function App() {
+  const [getParams] = useSearchParams();
+  const { setPromo, clearPromo } = usePromoStore();
+
+  const code = getParams.get("code");
+  const type = getParams.get("type");
+
+  const { data } = useQuery({
+    queryKey: ["Promotion", code, type],
+    queryFn: () => getPromotion(code!, type!),
+    enabled: !!code && !!type,
+  });
+
+  useEffect(() => {
+    if (!code && !type) {
+      clearPromo();
+
+      return;
+    }
+
+    if (!data) return clearPromo();
+
+    if (data.status === 200) {
+      setPromo(data.data.data);
+      toast.success("Enjoy Your Promotion");
+    } else {
+      clearPromo();
+    }
+  }, [code, type, data]);
+
   return (
     <>
       <MainLayout>
--- src/features/Cart/services.ts
+++ src/features/Cart/services.ts
@@ -16,10 +16,16 @@ export interface OrderInfo {
   deliveryFee: number;
   foodCost: number;
   totalPrice: number;
+  promoId?: number;
 }
 
 export const createOrder = async (formData: OrderInfo) => {
   const res = await api.post(`/api/orders/orders`, formData);
   const authorization_url = res.data.data.data.authorization_url;
   window.location.href = authorization_url;
+};
+
+export const getPromotion = async (code: string, type: string) => {
+  const res = await api.get(`/api/promotion?code=${code}&type=${type}`);
+  return res;
 };
