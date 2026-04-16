ae428264b3424de604de74241861f67c469f42f8
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-16 10:36:05 +0000

    Fix: Show home menu on home page if user is on vart page and vicversa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../assets/images/salad.png";
-import { Button } from "./ui/Button";
+// import { Button } from "./ui/Button";
 import { navData } from "../data/navData";
 import { motion } from "framer-motion";
 
 export const Navbar = () => {
   const path = useLocation();
+  const nav = useNavigate();
 
   return (
     <motion.nav
@@ -22,32 +23,41 @@ export const Navbar = () => {
           </h1>
         </Link>
 
-        {!path.pathname.startsWith("/cart/") && (
-          <div className="hidden md:flex items-center gap-6">
-            <Button text="Order Here" type="Submit" />
-          </div>
-        )}
-
-        {!path.pathname.startsWith("/cart/") && (
+        {!path.pathname.startsWith("/cart/") ||
+        path.pathname.startsWith("/cart/") ? (
           <div className="flex md:hidden overflow-x-auto gap-4 scrollbar-hide">
-            {navData.map((navItem) => (
-              <button
-                key={navItem.navText}
-                onClick={(e) => {
-                  e.preventDefault();
-                  const section = document.querySelector(navItem.navLink);
-                  section?.scrollIntoView({ behavior: "smooth" });
-                }}
-                className="shrink-0 text-secondary text-sm font-medium hover:text-primary"
-              >
-                <span className="flex items-center gap-x-1">
-                  {navItem.navIcon}
-                  {navItem.navText}
-                </span>
-              </button>
-            ))}
+            {navData
+              .filter((navItem) => {
+                if (path.pathname === "/" && navItem.navText === "Home") {
+                  return false;
+                }
+                if (path.pathname.startsWith("/cart/")) {
+                  return navItem.navText === "Home";
+                }
+                return true;
+              })
+              .map((navItem) => (
+                <div
+                  key={navItem.navText}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (navItem.navLink === "/") {
+                      nav("/");
+                    } else {
+                      const section = document.querySelector(navItem.navLink);
+                      section?.scrollIntoView({ behavior: "smooth" });
+                    }
+                  }}
+                  className="shrink-0 text-secondary text-sm font-medium hover:text-primary"
+                >
+                  <span className="flex items-center gap-x-1">
+                    {navItem.navIcon}
+                    {navItem.navText}
+                  </span>
+                </div>
+              ))}
           </div>
-        )}
+        ) : null}
       </div>
     </motion.nav>
   );
