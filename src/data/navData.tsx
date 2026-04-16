3c6d32e3667aef7724374cc5437b3fda7001fa0e
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-16 10:33:38 +0000

    Added menu to menudata

--- src/data/navData.tsx
+++ src/data/navData.tsx
@@ -1,9 +1,17 @@
 import { CgMenuBoxed } from "react-icons/cg";
+import { IoMdHome } from "react-icons/io";
 
 export const navData = [
   {
+    id: 1,
     navLink: "#menu",
     navText: "Menu",
     navIcon: <CgMenuBoxed />,
+  },
+  {
+    id: 2,
+    navLink: "/",
+    navText: "Home",
+    navIcon: <IoMdHome />,
   },
 ];
